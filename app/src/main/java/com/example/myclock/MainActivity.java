e68a2f64139679fc2310423917a5de560e8263c4
shw <1597189490@qq.com>
2023-10-12 20:49:41 +0800

    按照中国时区输出时间，绘制好电子时钟和更新时间的ui

--- app/src/main/java/com/example/myclock/MainActivity.java
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = findViewById(R.id.text1);
-        text.setText("");
+        TextView timeOut = findViewById(R.id.time);
+
+        TimeZone chinaTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        Date date = new Date();
+        SimpleDateFormat timeForm = new SimpleDateFormat("HH:mm:ss");
+        timeForm.setTimeZone(chinaTimeZone);
+
+        String time = timeForm.format(date);
+        timeOut.setText(time);
+
+        Button  refresh= findViewById(R.id.refresh);
+
+
+
 
 
     }
