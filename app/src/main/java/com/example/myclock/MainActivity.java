5ce6e41cd99583727f8786256697555b434abfa6
shw <1597189490@qq.com>
2023-10-13 12:02:34 +0800

    每秒更新一次效果实现

--- app/src/main/java/com/example/myclock/MainActivity.java
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,35 +13,42 @@ import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Handler handler = new Handler();
+    private final Runnable runable= new Runnable() {
+        @Override
+        public void run() {
+            TextView timeOut = findViewById(R.id.time);
+            timeOut.setText(getTimeForm());
+            handler.postDelayed(this,1000);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView timeOut = findViewById(R.id.time);
+        timeOut.setText(getTimeForm());
 
+        Button refresh = findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeOut.setText(getTimeForm());
+            }
+        });
+
+        handler.post(runable);
+    }
+
+    private String getTimeForm(){
         TimeZone chinaTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
         Date date = new Date();
         SimpleDateFormat timeForm = new SimpleDateFormat("HH:mm:ss");
         timeForm.setTimeZone(chinaTimeZone);
 
-        String time = timeForm.format(date);
-        timeOut.setText(time);
-
-        Button refresh= findViewById(R.id.refresh);
-        refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TimeZone chinaTimeZone = TimeZone.getTimeZone("Asia/Shanghai");
-                Date date = new Date();
-                SimpleDateFormat timeForm = new SimpleDateFormat("HH:mm:ss");
-                timeForm.setTimeZone(chinaTimeZone);
-                String time = timeForm.format(date);
-                timeOut.setText(time);
-            }
-        });
-
-
-
+        return timeForm.format(date);
     }
+
 }
